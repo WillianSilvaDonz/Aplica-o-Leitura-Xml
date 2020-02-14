b6e1f2159536f1e98230cf973434da122352de47
Willian Maikon Da Silva <willian.silva@new.net>
2020-02-14 15:48:44 -0200

    add project send

---
+++ TesteEmissaoView/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TesteEmissaoView.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
